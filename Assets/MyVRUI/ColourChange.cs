ce0fc2680f3941ad777c93f7549a7cd9fa8aa5a3
Ben Ryan <ben2.ryan@live.uwe.ac.uk>
2018-04-23 14:05:18 +0100

    Initial Commit
    VRUI with controller support

---
+++ Assets/MyVRUI/ColourChange.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourChange : MonoBehaviour {
+
+    public float red, green, blue;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RedSlider(float value)
+    {
+        this.red = value;
+        SetColour();
+    }
+    public void GreenSlider(float value)
+    {
+        this.green = value;
+        SetColour();
+    }
+    public void BlueSlider(float value)
+    {
+        this.blue = value;
+        SetColour();
+    }
+
+    public void SetColour()
+    {
+        GetComponent<Renderer>().sharedMaterial.color = new Color(red, green, blue);
+    }
+}
