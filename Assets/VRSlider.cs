926f07ed087f669c1146c3fe9d599a012b894414
Ben Ryan <ben2.ryan@live.uwe.ac.uk>
2018-04-26 02:30:58 +0100

    The end is nigh
    small changes, as nothing worked.

--- Assets/VRSlider.cs
+++ Assets/VRSlider.cs
@@ -15,6 +15,19 @@ public class VRSlider : MonoBehaviour {
     [SerializeField] private Renderer m_HRenderer;
 
     public Slider mySlider;
+    private float lastKnownPos;
+    private Transform Track;
+
+    public Transform handlePos;
+    private Vector3 targetPos;
+    private Vector3 neutralPos;
+
+    public bool clicked = false;
+
+    private float sliderPercent;
+    private float sliderLength;
+
+    
 
     private void Awake()
     {
@@ -22,6 +35,11 @@ public class VRSlider : MonoBehaviour {
         m_HRenderer.material = m_NormalMaterial;
     }
 
+    private void Start()
+    {
+        targetPos = handlePos.position;
+        sliderLength = GetComponent<BoxCollider>().size.x;
+    }
 
     private void OnEnable()
     {
@@ -45,7 +63,16 @@ public class VRSlider : MonoBehaviour {
     private void HandleOver()
     {
         Debug.Log("Show over state");
-        
+        //attempted to use the difference in current position and last know position to adjust the value of the slider
+        //if(clicked == true)
+        //{
+        //    var pos = GameObject.Find("Tracking").transform.position.x;
+        //    //attempted to find the position or rotation of the controller
+        //    float rot = pos;
+        //    // my slider value equals change in wand x value
+        //    mySlider.value = pos - lastKnownPos;
+        //}
+        //lastKnownPos = GameObject.Find("Tracking").transform.position.x;
         m_HRenderer.material = m_OverMaterial;
     }
 
@@ -62,7 +89,12 @@ public class VRSlider : MonoBehaviour {
     private void HandleClick()
     {
         Debug.Log("Show click state");
-
+        // to change bool from tru to false, once clicked
+        //clicked = !clicked;
+        // Moves the slider handle to the target position
+        //targetPos = new Vector3(neutralPos.x, neutralPos.y, targetPos.z);
+        //handlePos.position = Vector3.Lerp(handlePos.position, targetPos, Time.deltaTime * 7);
+        
         mySlider.value++;
 
         m_HRenderer.material = m_ClickedMaterial;
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Generalisation-of-VRUI-15015673
+Student ID: 15015673
+Student Name: Ben Ryan
+
